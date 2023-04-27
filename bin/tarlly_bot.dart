1ef715b0e841bafec39a4809bd468eb421170d12
Tarllysson Silva <tharllysonsilva@gmail.com>
2023-04-26 21:23:00 -0300

    Nome do BOT

--- bin/tarlly_bot.dart
+++ bin/tarlly_bot.dart
@@ -1,14 +1,13 @@
-
 import 'dart:io';
 
 import 'questions/time_questions.dart';
 
 void main() async {
-  String kakoBot = 'KakoBOT:\n';
+  String tarllyBot = 'tarllyBot:\n';
   var a = true;
   String usuario = '';
 
-  print('-- Iniciando o KakoBOT, aguarde..--');
+  print('-- Iniciando o tarllyBot, aguarde..--');
 
   print('KakoBOT:\n Oi :) \n Como posso ajudar?');
   do {
@@ -19,16 +18,16 @@ void main() async {
         usuario.contains('Adeus') ||
         usuario.contains('adeus')) {
       a = false;
-      print(kakoBot + ' Até a proxima!!');
+      print(tarllyBot + ' Até a proxima!!');
     } else if (TimeQuestions(usuario).isThisTime()) {
       // verificar antes, assim não fazemos toda a função sem precisar.
       TimeQuestions(usuario).timeQuestion();
     } else if (false) {
       //Basta adicionar novas perguntas aqui!
     } else {
-      print(kakoBot +
+      print(tarllyBot +
           ' Não fui treinado para responder a essa pergunta \n Desculpe :( ');
-      print(kakoBot + ' Você pode fazer outra pergunta ou dizer Adeus');
+      print(tarllyBot + ' Você pode fazer outra pergunta ou dizer Adeus');
     }
   } while (a);
 
