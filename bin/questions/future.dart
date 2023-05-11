dd891cb770d031eb0bf7586bdd4ce34494f1f1ea
Tarllysson Silva <tharllysonsilva@gmail.com>
2023-05-11 17:54:13 -0300

    Async & Await

--- bin/questions/future.dart
+++
@@ -1,8 +0,0 @@
-void main() {
-  Future myFuture = Future(() {
-    print('Going back to the future');
-    return 21;
-  }).then((value) => print('The value is $value'));
-
-  print('Done with main');
-}
