7a0515c725aa5bc2d76e5bc897204e4570adbe6d
Tarllysson Silva <tharllysonsilva@gmail.com>
2023-05-17 00:00:49 -0300

    Tratamento de error

--- bin/future.dart
+++ bin/future.dart
@@ -16,7 +16,26 @@ void main() {
   myFutureFunc()
       .then((value) => print('My function value is: $value'))
       //Tratamento de erro
-      .onError((error, stackTrace) => print('An Error Accurred'));
+      .onError((error, stackTrace) => print('An Error Accurred'))
+      // Conclui quando terminar tudo
+      .whenComplete(() => print('The Future is Over'));
+
+  // Tratamento de prevenções a erros
+  Future<int> myFutureErrorFunc(int a, int b) async {
+    try {
+      if (a > b) {
+        throw Exception();
+      }
+      print('I have a functional Function');
+      await Future.delayed(Duration(seconds: 5));
+      return 42;
+    } catch (e) {
+      print('An Error occurred: $e');
+      return 42;
+    } finally {
+      print('The Future Finally is Over');
+    }
+  }
 
   print('Done with main');
 }
