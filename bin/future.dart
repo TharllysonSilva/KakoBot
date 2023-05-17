85caef7309d46d0f23d88a19cfa5807e03816cce
Tarllysson Silva <tharllysonsilva@gmail.com>
2023-05-17 00:00:59 -0300

    Previnindo erros

--- bin/future.dart
+++ bin/future.dart
@@ -1,24 +1,24 @@
 void main() {
-  Future myFuture = Future(() {
-    print('Going back to the future');
-    return 21;
-  }).then((value) => print('The value is $value'));
+  // Future myFuture = Future(() {
+  // print('Going back to the future');
+  // return 21;
+  // }).then((value) => print('The value is $value'));
 
   // Habilitando a função ser assincrona
   Future<int> myFutureFunc() async {
     print('I have a function in the Future');
     //Para poder esperar o tempo de ducação declarado
     await Future.delayed(Duration(seconds: 5));
-    throw Exception();
+    //throw Exception();
     return 12;
   }
 
-  myFutureFunc()
-      .then((value) => print('My function value is: $value'))
-      //Tratamento de erro
-      .onError((error, stackTrace) => print('An Error Accurred'))
-      // Conclui quando terminar tudo
-      .whenComplete(() => print('The Future is Over'));
+  // myFutureFunc()
+  //   .then((value) => print('My function value is: $value'))
+  //Tratamento de erro
+  //  .onError((error, stackTrace) => print('An Error Accurred'))
+  // Conclui quando terminar tudo
+  //   .whenComplete(() => print('The Future is Over'));
 
   // Tratamento de prevenções a erros
   Future<int> myFutureErrorFunc(int a, int b) async {
@@ -37,5 +37,6 @@ void main() {
     }
   }
 
+  myFutureErrorFunc(1, 2).then((value) => print('The value is : $value'));
   print('Done with main');
 }
