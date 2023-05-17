ce78babc14080dfa3673b63fe10995dd93f8c131
Tarllysson Silva <tharllysonsilva@gmail.com>
2023-05-16 23:19:55 -0300

    onError

--- bin/future.dart
+++ bin/future.dart
@@ -9,10 +9,13 @@ void main() {
     print('I have a function in the Future');
     //Para poder esperar o tempo de ducação declarado
     await Future.delayed(Duration(seconds: 5));
+    throw Exception();
     return 12;
   }
 
-  myFutureFunc().then((value) => print('My function value is: $value'));
+  myFutureFunc()
+      .then((value) => print('My function value is: $value'))
+      .onError((error, stackTrace) => print('An Error Accurred'));
 
   print('Done with main');
 }
