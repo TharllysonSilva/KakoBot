29f292faac213b53f04f8487b86df37140bcf5db
Tarllysson Silva <tharllysonsilva@gmail.com>
2023-05-31 00:28:41 -0300

    Treinando o bot

--- bin/questions/good_manners.dart
+++ bin/questions/good_manners.dart
@@ -23,4 +23,26 @@ class GoodManners {
     }
     return isThisGoodManners;
   }
+
+  goodManner() {
+    if (question.contains('Bom dia') || question.contains('bom dia')) {
+      print(tarllyBot + 'Bom dia Flor do dia!');
+    } else if (question.contains('Boa tarde') ||
+        question.contains('boa tarde')) {
+      print(tarllyBot + 'Opa siow, boa tarde');
+    } else if (question.contains('Boa noie') ||
+        question.contains('boa noite')) {
+      print(tarllyBot + 'Uma bela noite, não é mesmo?');
+    } else if (question.contains('oi') ||
+        question.contains('Oi') ||
+        question.contains('Olá') ||
+        question.contains('olá')) {
+      print(tarllyBot + 'Muito prazer!');
+    } else if (question.contains('Obrigado') ||
+        question.contains('obrigado') ||
+        question.contains('Obrigada') ||
+        question.contains('obrigada')) {
+      print(tarllyBot + 'De nada, disponha c:');
+    }
+  }
 }
